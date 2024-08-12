97270c4b729f87dbede7dfb31d55d682ef26a3a0
adilet <orozaliev.adilet.07@gmail.com>
2024-08-12 07:10:57 +0600

    first parallax 90% done

---
+++ GameSearchProject/js/parallax.js
@@ -0,0 +1,29 @@
+// HEADER
+const $header = document.querySelector('.header');
+const defaultOffset = 100;
+let lastScroll = 0;
+
+const scrollPosition = () => window.scrollY || document.documentElement.scrollTop;
+const isHidden = () => $header.classList.contains('hide');
+
+window.addEventListener('scroll', () => {
+
+    if(scrollPosition() > lastScroll && !isHidden() && scrollPosition() > defaultOffset) {
+        $header.classList.add('hide');
+    } else if (scrollPosition() < lastScroll && isHidden()){
+        $header.classList.remove('hide')
+    }
+    lastScroll = scrollPosition();
+});
+
+// NEW
+window.addEventListener('scroll', () => {
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`; // this т.к. window нужен
+});
+
+// GSAP
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
+});
